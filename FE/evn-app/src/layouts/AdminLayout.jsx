43df29b2a5c6c765b29c28defc672d210b308c30
ThienNguyen227 <nguyenhoangnhutthien6666@gmail.com>
2026-03-31 01:42:02 +0700

    Add role + username

--- FE/evn-app/src/layouts/AdminLayout.jsx
+++ FE/evn-app/src/layouts/AdminLayout.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar"; // <-- import Sidebar từ folder Components
 // import "../styles/adminLayout.css"; // nếu có CSS riêng
 import Header from "../components/Header";
 
-export default function AdminLayout({ children }) {
+export default function AdminLayout({ username, role}) {
   return (
-    <div className="admin-layout">
-      <Header username="Admin" />
-      <Sidebar role="Admin"/> {/* Sidebar sẽ luôn hiển thị bên trái */}
-      <div className="admin-content">
-        {children} {/* Nội dung trang sẽ được render ở đây */}
+    <div className="h-screen flex flex-col">
+      {/* Header */}
+      <Header username={username} />
+    
+      {/* Body */}
+      <div className="flex flex-1 overflow-hidden">
+        
+        {/* Sidebar */}
+        <div className="w-64 bg-gray-100">
+          <Sidebar role={role} />
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-1 p-4 bg-gray-50 overflow-auto">
+          <Outlet />
+        </div>
+
       </div>
     </div>
   );
