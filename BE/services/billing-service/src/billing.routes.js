70d38fd5ea2a5d1b462863e069a5815ccf1f5b50
ThienNguyen227 <nguyenhoangnhutthien6666@gmail.com>
2026-05-05 01:24:31 +0700

    add generate invoice route

--- BE/services/billing-service/src/billing.routes.js
+++ BE/services/billing-service/src/billing.routes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const getInvoicesRoute = require("./billing-modules/get_invoices.module");
+const generateInvoicesRoute = require("./billing-modules/generate_invoices.module");
 
 router.use("/invoices", getInvoicesRoute);
+router.use("/generates", generateInvoicesRoute);
+
+
 
 module.exports = router;
